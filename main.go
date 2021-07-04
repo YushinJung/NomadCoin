becfb28328a6cfe851774ee14967f4787edaceee
jyushin90@gmail.com <jyushin90@gmail.com>
2021-07-04 21:41:12 +0900

    adding blocks

--- main.go
+++ main.go
@@ -16,9 +16,38 @@ type block struct {
 // if the data of hash is changed in the middle
 // all of the hash will be changed after that hash
 
+type blockchain struct {
+	blocks []block
+}
+
+func (b *blockchain) getLastHash() string {
+	if len(b.blocks) > 0 {
+		return b.blocks[len(b.blocks)-1].hash
+	} else {
+		return ""
+	}
+}
+func (b *blockchain) addBlock(data string) {
+	newBlock := block{data, "", b.getLastHash()}
+	hash := sha256.Sum256([]byte(newBlock.data + newBlock.prevHash))
+	newBlock.hash = fmt.Sprintf("%x", hash)
+	b.blocks = append(b.blocks, newBlock)
+}
+
+func (b *blockchain) listBlocks() {
+	for _, block := range b.blocks {
+		fmt.Printf("Data: %s\n", block.data)
+		fmt.Printf("Hash: %s\n", block.hash)
+		fmt.Printf("Prev Hash: %s\n", block.prevHash)
+	}
+}
+
 func main() {
-	genesisBlock := block{"Genesis Block", "", ""}
-	hash := sha256.Sum256([]byte(genesisBlock.data + genesisBlock.prevHash))
-	genesisBlock.hash = fmt.Sprintf("%x", hash)
-	fmt.Println(genesisBlock)
+	chain := blockchain{}
+
+	chain.addBlock("Genesis Block")
+	chain.addBlock("Second Block")
+	chain.addBlock("Third Block")
+	chain.listBlocks()
+
 }
