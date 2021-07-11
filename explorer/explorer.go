227c41aba72abb1b3e36c560be6b9dd5c39dface
jyushin90@gmail.com <jyushin90@gmail.com>
2021-07-11 17:20:42 +0900

    HTML - web hosting divide html as page and template used text/template, net/http for easily handling in/output

---
+++ explorer/explorer.go
@@ -0,0 +1,57 @@
+package explorer
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/YushinJung/NomadCoin/blockchain"
+)
+
+var templates *template.Template
+
+const (
+	port        string = ":4000"
+	templateDir string = "explorer/templates/"
+)
+
+//parsing 파일을 하기 전에 function 이 호출되기 전에 template(pages, partials)을 일단 loading 해 놓자
+//이를 하기 위해 template에 대한 정보를 갖고 있는 variable을 선언하자.
+
+type homeData struct {
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
+
+func home(rw http.ResponseWriter, r *http.Request) {
+	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
+	templates.ExecuteTemplate(rw, "home", data)
+	//templ := template.Must(template.ParseFiles("templates/pages/home.gohtml"))
+	//templ.Execute(rw, data)
+}
+
+func add(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		templates.ExecuteTemplate(rw, "add", nil)
+	case "POST":
+		r.ParseForm()
+		data := r.Form.Get("blockData")
+		blockchain.GetBlockchain().AddBlock(data)
+		//redirection
+		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
+	}
+}
+
+func Start() {
+	//함수로 부르지 않고 저장되어 있는 template을 가져올 것이다
+	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
+	// loaded all file in "pages folder"
+	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
+	// templates 는 template Object이고, 다른 template를 load 할 수 있음.
+	http.HandleFunc("/", home)
+	http.HandleFunc("/add", add)
+	fmt.Printf("listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil)) // log.Fatal will stop if there is error from input
+}
--- main.go
+++ main.go
@@ -1,36 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-	"text/template"
+import "github.com/YushinJung/NomadCoin/explorer"
 
-	"github.com/YushinJung/NomadCoin/blockchain"
-)
-
-const port string = ":4000"
-
-type homeData struct {
-	PageTitle string
-	Blocks    []*blockchain.Block
-}
-
-func home(rw http.ResponseWriter, r *http.Request) {
-	// to write somewhere (small)
-	// read data (big)
-
-	templ := template.Must(template.ParseFiles("templates/home.gohtml"))
-	// templ, err := template.ParseFiles("templates/home.html")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
-	templ.Execute(rw, data)
-}
 func main() {
-	// http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {})
-	http.HandleFunc("/", home)
-	fmt.Printf("listening on http://localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil)) // log.Fatal will stop if there is error from input
+	explorer.Start()
 }
