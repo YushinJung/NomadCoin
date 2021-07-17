53dfc6e8905b3c779f672a15f2d3c8a580931c09
jyushin90@gmail.com <jyushin90@gmail.com>
2021-07-17 17:23:53 +0900

    JSON-API - repackage to "rest"

--- main.go
+++ main.go
@@ -1,72 +1,7 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/YushinJung/NomadCoin/blockchain"
-	"github.com/YushinJung/NomadCoin/utils"
-)
-
-const port string = ":4000"
-
-type URL string
-
-func (u URL) MarshalText() ([]byte, error) {
-	url := fmt.Sprintf("http://localhost%s%s", port, u)
-	return []byte(url), nil
-}
-
-type URLDescription struct {
-	URL         URL    `json:"url"`
-	Method      string `json:"method"`
-	Description string `json:"description"`
-	Payload     string `json:"payload,omitempty"`
-}
-type AddBlockBody struct {
-	Message string
-}
-
-func documentation(rw http.ResponseWriter, r *http.Request) {
-	data := []URLDescription{
-		{
-			URL:         URL("/"),
-			Method:      "GET",
-			Description: "See Documentation",
-		},
-		{
-			URL:         URL("/blocks"),
-			Method:      "POST",
-			Description: "Add A Block",
-			Payload:     "data:string",
-		},
-	}
-	rw.Header().Add("Content-Type", "application/json")
-	// b, err := json.Marshal(data)
-	// utils.HandleErr(err)
-	// fmt.Fprintf(rw, "%s", b)
-	json.NewEncoder(rw).Encode(data)
-}
-
-func blocks(rw http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		rw.Header().Add("Content-Type", "application/json")
-		json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
-	case "POST":
-		var addBlockBody AddBlockBody
-		utils.HandleErr(json.NewDecoder(r.Body).Decode(&addBlockBody))
-		// r.Body 에서 받아와서 addBlockBody 에 넣을 것
-		blockchain.GetBlockchain().AddBlock(addBlockBody.Message)
-		rw.WriteHeader(http.StatusCreated)
-	}
-}
+import "github.com/YushinJung/NomadCoin/rest"
 
 func main() {
-	http.HandleFunc("/", documentation)
-	http.HandleFunc("/blocks", blocks)
-	fmt.Printf("Listening on http://localhost%s", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	rest.Start()
 }
---
+++ rest/rest.go
@@ -0,0 +1,77 @@
+package rest
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/YushinJung/NomadCoin/blockchain"
+	"github.com/YushinJung/NomadCoin/utils"
+)
+
+const port string = ":4000"
+
+type url string
+
+func (u url) MarshalText() ([]byte, error) {
+	url := fmt.Sprintf("http://localhost%s%s", port, u)
+	return []byte(url), nil
+}
+
+type urlDescription struct {
+	URL         url    `json:"url"`
+	Method      string `json:"method"`
+	Description string `json:"description"`
+	Payload     string `json:"payload,omitempty"`
+}
+type addBlockBody struct {
+	Message string
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []urlDescription{
+		{
+			URL:         url("/"),
+			Method:      "GET",
+			Description: "See Documentation",
+		},
+		{
+			URL:         url("/blocks"),
+			Method:      "POST",
+			Description: "Add A Block",
+			Payload:     "data:string",
+		},
+		{
+			URL:         url("/blocks/{id}"),
+			Method:      "GET",
+			Description: "See A Block",
+		},
+	}
+	rw.Header().Add("Content-Type", "application/json")
+	// b, err := json.Marshal(data)
+	// utils.HandleErr(err)
+	// fmt.Fprintf(rw, "%s", b)
+	json.NewEncoder(rw).Encode(data)
+}
+
+func blocks(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		rw.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
+	case "POST":
+		var aBB addBlockBody
+		utils.HandleErr(json.NewDecoder(r.Body).Decode(&aBB))
+		// r.Body 에서 받아와서 addBlockBody 에 넣을 것
+		blockchain.GetBlockchain().AddBlock(aBB.Message)
+		rw.WriteHeader(http.StatusCreated)
+	}
+}
+
+func Start() {
+	http.HandleFunc("/", documentation)
+	http.HandleFunc("/blocks", blocks)
+	fmt.Printf("Listening on http://localhost%s", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
