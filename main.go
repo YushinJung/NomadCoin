3e670655f9ea01105ff9a33f433f7a9179ea43d3
jyushin90@gmail.com <jyushin90@gmail.com>
2021-07-18 11:53:20 +0900

    CLI-argument parsing flatset

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
-	"github.com/YushinJung/NomadCoin/rest"
+	"flag"
+	"fmt"
+	"os"
 )
 
+func usage() {
+	fmt.Printf("Welcome to Yushin Coin\n\n")
+	fmt.Printf("Pelase use the following commands:\n\n")
+	fmt.Printf("explorer: 	Start the HTML Explore\n")
+	fmt.Printf("rest:		Start the REST API (recommended)\n\n")
+	os.Exit(0)
+}
+
 func main() {
-	rest.Start(4000)
+	if len(os.Args) < 2 {
+		usage()
+	}
+
+	rest := flag.NewFlagSet("rest", flag.ExitOnError)
+	portFlag := rest.Int("port", 4000, "Sets the port of the server")
+
+	switch os.Args[1] {
+	case "explorer":
+		fmt.Println("Start Explorer")
+	case "rest":
+		rest.Parse(os.Args[2:])
+		fmt.Println("Start REST API")
+	default:
+		usage()
+	}
+	fmt.Println(*portFlag)
 }
