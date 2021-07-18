59278b391978a07a6dc0ec04a6d13419e7edc36b
jyushin90@gmail.com <jyushin90@gmail.com>
2021-07-18 12:09:04 +0900

    CLI-argument parsing with normal flag refactoring

---
+++ cli/cli.go
@@ -0,0 +1,37 @@
+package cli
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/YushinJung/NomadCoin/explorer"
+	"github.com/YushinJung/NomadCoin/rest"
+)
+
+func usage() {
+	fmt.Printf("Welcome to Yushin Coin\n\n")
+	fmt.Printf("Pelase use the following flags:\n\n")
+	fmt.Printf("-port=4000: 	Set the PORT of the server\n")
+	fmt.Printf("-mode=rest		'html' vs 'rest'\n\n")
+	os.Exit(0)
+}
+
+func Start() {
+	port := flag.Int("port", 4000, "Set port of the server")
+	mode := flag.String("mode", "rest", "'html' vs 'rest'")
+
+	flag.Parse()
+
+	fmt.Println(*port, *mode)
+
+	switch *mode {
+	case "rest":
+		// start with rest api
+		rest.Start(*port)
+	case "html":
+		explorer.Start(*port)
+	default:
+		usage()
+	}
+}
--- main.go
+++ main.go
@@ -1,35 +1,7 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"os"
-)
-
-func usage() {
-	fmt.Printf("Welcome to Yushin Coin\n\n")
-	fmt.Printf("Pelase use the following commands:\n\n")
-	fmt.Printf("explorer: 	Start the HTML Explore\n")
-	fmt.Printf("rest:		Start the REST API (recommended)\n\n")
-	os.Exit(0)
-}
+import "github.com/YushinJung/NomadCoin/cli"
 
 func main() {
-	if len(os.Args) < 2 {
-		usage()
-	}
-
-	rest := flag.NewFlagSet("rest", flag.ExitOnError)
-	portFlag := rest.Int("port", 4000, "Sets the port of the server")
-
-	switch os.Args[1] {
-	case "explorer":
-		fmt.Println("Start Explorer")
-	case "rest":
-		rest.Parse(os.Args[2:])
-		fmt.Println("Start REST API")
-	default:
-		usage()
-	}
-	fmt.Println(*portFlag)
+	cli.Start()
 }
